0702b4093068f05649e86f9b144536f343be07f1
Mike <67948209+mikegirardi@users.noreply.github.com>
2023-04-14 10:08:06 +0100

    Update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,10 +1,17 @@
+#Write a program to calculate how many months it will take you to save up enough money for a down payment.
+#You will want your main variables to be floats, so you should cast user inputs to floats.
+#Your program should ask the user to enter the following variables:
+#1. The starting annual salary (annual_salary)
+#2. The portion of salary to be saved (portion_saved)
+#3. The cost of your dream home (total_cost)
+
 current_savings = 0
 r = 0.04
 months = 0
 
-annual_salary = int(input("Enter your annual salary: "))
+annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-total_cost = int(input("Enter the cost of your dream home: "))
+total_cost = float(input("Enter the cost of your dream home: "))
 portion_down_payment = total_cost * 0.25
 
 while current_savings < portion_down_payment:
